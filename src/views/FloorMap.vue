fa7c7a3a2ae016d57674ce8f09b90eb7b337832b
knowlet <knowlet3389@gmail.com>
2019-11-06 03:41:10 +0800

    Upload images

--- src/views/FloorMap.vue
+++ src/views/FloorMap.vue
@@ -7,12 +7,6 @@
           <img src="@/assets/images/floormap/2F-map.png" alt="">
         </div>
       </div>
-      <div class="spotlight-box">
-        <h1 class="title symbol"><span>3F 場地圖</span></h1>
-        <div class="content">
-          <img src="@/assets/images/floormap/3F-map.png" alt="">
-        </div>
-      </div>
     </div>
     <Footer />
   </div>
