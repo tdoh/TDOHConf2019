ed88d81e3b624d89bc38367fe22d7ef105a11ac6
knowlet <knowlet3389@gmail.com>
2019-11-12 01:03:23 +0800

    Update time and location

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,12 +4,12 @@
       <div class="container">
         <img id="logo" src="../assets/images/logo-big.png" alt="">
         <div class="content">
-          <p class="tag">9 / 29 10:00 ~ 16:30</p>
+          <p class="tag">11 / 30 11:00 ~ 17:30</p>
           <div class="title-box">
             <h2>TDOH Conf</h2>
             <h1>2019</h1>
             <h3>駭客地下城</h3>
-            <p>地點：<a href="https://ppt.cc/fgKQWx">高雄蓮潭國際會館</a></p>
+            <p>地點：<a href="https://g.page/janddbrewing?share">吉姆老爹啤酒工場</a></p>
           </div>
           <a href="https://tdohackerparty.kktix.cc/events/tdoh-conf-2019" class="button" target="_blank">立即報名</a>
           <Counter :endDate="endDate" v-if="endDate.getTime() > new Date().getTime()" />
