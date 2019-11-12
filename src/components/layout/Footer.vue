6474bf9cf3ea6404d9bfc2027f6ef58d61afc1ae
knowlet <knowlet3389@gmail.com>
2019-11-12 23:37:05 +0800

    Update corporation name

--- src/components/layout/Footer.vue
+++ src/components/layout/Footer.vue
@@ -2,7 +2,7 @@
   <div id="footer">
     <p>&copy; TDOHacker 2019</p>
     <p>主辦單位｜TDOHacker</p>
-    <p>承辦單位｜臺灣校園資訊安全推廣暨駭客培育協會</p>
+    <p>承辦單位｜社團法人臺灣校園資訊安全推廣暨駭客培育協會</p>
     <p>技術協力｜<a href="https://open.limeishu.org.tw">LIT Team</a></p>
   </div>
 </template>
